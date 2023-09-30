547a85d376de0ca8f8211565b87f39631407ef33
IgorL123 <Zedenbraucht@inbox.ru>
2023-10-01 01:23:17 +0300

    emb & tests

--- app/backend/core/vectordb.py
+++ app/backend/core/vectordb.py
@@ -1,18 +1,26 @@
 import chromadb
 from chromadb.config import Settings
 from chromadb import Documents, EmbeddingFunction, Embeddings
+from chromadb.utils import embedding_functions
 from .utils import fasttext
+import numpy as np
+from typing import cast
+
+client = None
 
 
 class FasttextEmbeddings(EmbeddingFunction):
+    """
+    Cant work with english - only russian
+    """
     def __call__(self, texts: Documents) -> Embeddings:
-        embeddings = []
-        for doc in texts:
-            embeddings.append(fasttext(doc))
-        return embeddings
+        return cast(Embeddings, [
+            fasttext(text).astype(np.float32) for text in texts
+        ])
 
 
 def create_client():
+    global client
     """
     Create Croma client and main collection
     :return:
@@ -20,11 +28,19 @@ def create_client():
     settings = Settings(anonymized_telemetry=False)
     client = chromadb.Client(settings)
 
+    # emb_fn = FasttextEmbeddings()
+    emb_fn = embedding_functions.DefaultEmbeddingFunction()
     collection = client.create_collection(
         name="main_app_collection",
-        embedding_function=FasttextEmbeddings,
+        embedding_function=emb_fn,
         metadata={"hnsw:space": "cosine"},
         get_or_create=True,
     )
 
     return collection
+
+
+def stop_client():
+    global client
+    if client:
+        client.stop()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,11 @@
 import pytest
+from .test_backend import client
 from app.backend.core.models import FakeModel
+from app.backend.core.vectordb import create_client, stop_client
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from app.backend.core.utils import fasttext
+from numpy import float32, issubdtype
 
 
 def test_fake_model():
@@ -21,3 +25,55 @@ def test_fake_model():
     assert completion.find("Tell") != -1
 
 
+def test_fasttext(client):
+    doc = "Съешь же ещё этих мягких французских булок, да выпей чаю"
+    emb = fasttext(doc)
+    assert len(emb) == 300
+    assert issubdtype(emb.dtype, float32)
+
+
+def test_chroma(client):
+
+    doc1 = "Natural Language Processing (NLP) is a domain of research whose objective is to analyze and understand it"
+    doc2 = "Съешь же ещё этих мягких французских булок, да выпей чаю"
+    doc3 = "Обычно панграммы используют для презентации шрифтов, чтобы можно было в одной фразе рассмотреть все глифы"
+    test_text = "We introduce this question answering NLP project"
+
+    collection = create_client()
+    collection.add(
+        documents=[
+            doc1,
+            doc2,
+            doc3,
+        ],
+        metadatas=[
+            {"testing": True},
+            {"testing": True},
+            {"testing": True},
+        ],
+        ids=[
+            "1",
+            "2",
+            "3",
+        ]
+    )
+
+    texts = collection.query(
+        query_texts=[test_text],
+        n_results=1,
+    )
+    assert texts["documents"][0][0] == doc1
+
+    texts = collection.get(
+        where={"testing": True}
+    )
+    assert len(texts["documents"]) == 3
+    collection.delete(
+        where={"testing": True}
+    )
+    texts = collection.get(
+        where={"testing": True}
+    )
+    assert len(texts["documents"]) == 0
+
+    stop_client()
